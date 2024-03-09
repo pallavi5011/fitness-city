e79c7219d319299a0cb2a8a301cb2db9ab8fa44a
pallavi5011 <pallawi758@gmail.com>
2024-03-10 01:02:42 +0530

    Reactive-form

--- src/app/create-registration/create-registration.component.ts
+++ src/app/create-registration/create-registration.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatRadioModule} from '@angular/material/radio';
+import { CommonModule } from '@angular/common'
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create-registration',
@@ -11,10 +17,80 @@ import {MatRadioModule} from '@angular/material/radio';
     MatInputModule,
     MatIconModule,
     MatRadioModule,
+    CommonModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatButtonModule,
   ],
   templateUrl: './create-registration.component.html',
   styleUrl: './create-registration.component.scss'
 })
-export class CreateRegistrationComponent {
+export class CreateRegistrationComponent implements OnInit{
+  public packages = ["Monthly","Quarterly","Yearly"];
+  public impOpts : string[] = [
+    "Toxic Fat reduction",
+    "Energy and Endurance",
+    "Building Lean Muscle",
+    "Healthier Digestive System",
+    "Sugar Craving Body",
+    "Fitness"
+  ];
+
+  public registerForm!: FormGroup;
+
+  constructor (private fb:FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      mobile: [''],
+      weight: [''],
+      height: [''],
+      bmi: [''],
+      bmiResult: [''],
+      requireTrainer: [''],
+      gender: [''],
+      package: [''],
+      important: [''],
+      haveGymBefore: [''],
+      enquiryDate: [''],
+    })
+  }
+
+
+  submit(){
+    console.log("all details", this.registerForm.value)
+  }
+
+  calcBmi(heightValue:number){
+    const weight = this.registerForm.value.height;
+    const height = heightValue;
+    const bmi = weight / (height*height);
+    this.registerForm.controls['bmi'].patchValue(bmi);
+
+    switch (true){
+      case bmi <18.5:
+        this.registerForm.controls['bmiResult'].patchValue("Underweight");
+        break;
+        case (bmi >= 18.5 && bmi < 25):
+        this.registerForm.controls['bmiResult'].patchValue("Normal");
+        break;
+        case (bmi >= 18.5 && bmi <30):
+        this.registerForm.controls['bmiResult'].patchValue("Overweight");
+        break;
+
+        default:
+        this.registerForm.controls['bmiResult'].patchValue("Obese");
+        break;
+
+    }
+  }
+
 
 }
